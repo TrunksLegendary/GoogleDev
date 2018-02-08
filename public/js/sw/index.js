71ed113e693d5ba7e8ddb440d6a6beeb9f7ab7cd
TrunksLegendary <trunks0408@gmail.com>
2018-02-08 16:06:48 -0500

    git - Sync
    sync

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,10 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  // TODO: Respond to all requests with a HTML resopnse
-  // containing an element with a class="a-winnter-is-me".
-  // Ensure the Content-Type of the response id "text/html"
   event.respondWith(
-    new Response('Hello <b class="a-winner-is-me">world</b>',{
-      headers: {'Content-Type': 'text/html'}
+    new Response('Hello <b>World</b>', {
+      headers: {'foo': 'bar'}
     })
   );
 });
