38d8e1f0f4fcab480f6bd8edd4cf8f6039972aae
TrunksLegendary <trunks0408@gmail.com>
2018-02-07 23:25:21 -0500

    3.11 complete

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
+  // TODO: Respond to all requests with a HTML resopnse
+  // containing an element with a class="a-winnter-is-me".
+  // Ensure the Content-Type of the response id "text/html"
+  event.respondWith(
+    new Response('Hello <b class="a-winner-is-me">world</b>',{
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
