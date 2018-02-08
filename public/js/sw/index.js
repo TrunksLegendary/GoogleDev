22ea2f79d1dba1a7a881c93e76bcb3906d8c8856
TrunksLegendary <trunks0408@gmail.com>
2018-02-08 16:03:21 -0500

    3.13 Complete

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,7 @@
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.endsWith('.jpg'))  {
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-   );
-  }
+  if (event.request.url.endsWith('.jpg')) {
+      event.respondWith(
+        fetch('/imgs/dr-evil.gif')
+      );
+    }
 });
