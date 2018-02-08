04f7e4c290efcc2987dc0eaf6ab01850dc16739b
TrunksLegendary <trunks0408@gmail.com>
2018-02-08 18:10:30 -0500

    3.17 Complete

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -5,8 +5,18 @@ self.addEventListener('install', function(event) {
         '/',
         'js/main.js',
         'css/main.css',
-        'imgs/icon.png'
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
       ]);
     })
   );
-});  
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
