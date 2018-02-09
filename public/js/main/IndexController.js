c880f25bc5c9b433959ef793a266625a81276e28
TrunksLegendary <trunks0408@gmail.com>
2018-02-09 18:23:37 -0500

    update

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -8,57 +8,7 @@ export default function IndexController(container) {
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
   this._openSocket();
-  this._registerServiceWorker();
 }
-
-IndexController.prototype._registerServiceWorker = function() {
-  if (!navigator.serviceWorker) return;
-
-  var indexController = this;
-
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-    if (!navigator.serviceWorker.controller) {
-      return;
-    }
-
-    if (reg.waiting) {
-      indexController._updateReady(reg.waiting);
-      return;
-    }
-
-    if (reg.installing) {
-      indexController._trackInstalling(reg.installing);
-      return;
-    }
-
-    reg.addEventListener('updatefound', function() {
-      indexController._trackInstalling(reg.installing);
-    });
-  });
-
-  // TODO: listen for the controlling service worker changing
-  // and reload the page
-};
-
-IndexController.prototype._trackInstalling = function(worker) {
-  var indexController = this;
-  worker.addEventListener('statechange', function() {
-    if (worker.state == 'installed') {
-      indexController._updateReady(worker);
-    }
-  });
-};
-
-IndexController.prototype._updateReady = function(worker) {
-  var toast = this._toastsView.show("New version available", {
-    buttons: ['refresh', 'dismiss']
-  });
-
-  toast.answer.then(function(answer) {
-    if (answer != 'refresh') return;
-    // TODO: tell the service worker to skipWaiting
-  });
-};
 
 // open a connection to the server for live updates
 IndexController.prototype._openSocket = function() {
