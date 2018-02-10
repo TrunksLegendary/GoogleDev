97e1b6fc2a21aefc31c0011c3fa218ea495c9f33
TrunksLegendary <trunks0408@gmail.com>
2018-02-09 20:06:49 -0500

    3.18 Complete

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,26 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('wittr-static-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ]);
+    })
+  );
+});
+
+// 3.18 WIP
 self.addEventListener('fetch', function(event) {
-  if (event.request.url.endsWith('.jpg')) {
-      event.respondWith(
-        fetch('/imgs/dr-evil.gif')
-      );
-    }
+  // TODO: respond with an entry from the cache if there is one.
+  // If there isn't, fetch from the network.
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  )
 });
